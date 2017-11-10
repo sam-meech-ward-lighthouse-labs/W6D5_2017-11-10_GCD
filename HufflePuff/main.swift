7eafd8f36e9ca9e07cef72cc67a041b52a49bbc2
Sam Meech-Ward <sam@meech-ward.com>
2017-11-10 09:18:55 -0800

    Initial Commit

---
+++ HufflePuff/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HufflePuff
+//
+//  Created by Sam Meech-Ward on 2017-11-10.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
